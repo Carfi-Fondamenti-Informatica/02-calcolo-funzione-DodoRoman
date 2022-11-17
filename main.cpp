519b2de4a83bc8642858f71bcee93a4836c516ba
DodoRoman <115349003+DodoRoman@users.noreply.github.com>
2022-11-17 16:12:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+include <cmath>
 using namespace std;
 
 int main() {
+   float a=0, b=0, x=0, y=0;
+   cin >> a;
+   cin >> b;
+   cin >> x;
+   cin >> y;
+   float z=0;
+   if (x<0 && y>0){
+      z= a*x - b*y;
+      cout << z << endl;
+   }
+   else if (x>=0 && y<=0){
+      z= (a*pow(x,2)) - (b*y);
+      cout << z << endl;
+   }
+   else {
+      z= (a*x) + (b*pow(y,2));
+      cout << z << endl;
    return 0;
 }
